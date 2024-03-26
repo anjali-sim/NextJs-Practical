8b95d1dc066e074e5d3b66509debc164e56f6eec
anjali-sim <anjalighetia123@gmail.com>
2024-03-26 11:50:12 +0530

    :recycle: Made the signout page redirect to other page

--- app/(auth)/signOut/page.tsx
+++ app/(auth)/signOut/page.tsx
@@ -3,24 +3,25 @@
 import Button from "@/app/components/ui/Button/Button";
 import FormWrapper from "@/app/components/ui/Form/FormWrapper";
 import { signOut } from "next-auth/react";
-import { redirect } from "next/navigation";
 import { useRouter } from "next/navigation";
 
 export default function SignOut() {
-    const router = useRouter();
-    const handleSignOut = async () => {
-        await signOut();
-        // redirect("/");
-        router.push("/");
-    }
+  const router = useRouter();
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false, callbackUrl: "/login" });
+    router.push(data.url);
+  };
 
-    return (
-        <div>
-            <FormWrapper heading="Sign Out" content="Are you sure, you want to sign out?">
-                <div className="mb-6 flex justify-center">
-                <Button title="Sign Out" onClick={handleSignOut} />
-                </div>
-            </FormWrapper>
+  return (
+    <div>
+      <FormWrapper
+        heading="Sign Out"
+        content="Are you sure, you want to sign out?"
+      >
+        <div className="mb-6 flex justify-center">
+          <Button title="Sign Out" onClick={handleSignOut} />
         </div>
-    );
+      </FormWrapper>
+    </div>
+  );
 }
